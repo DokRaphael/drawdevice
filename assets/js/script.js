dc0f07001f2fae0b56bba575fd0f591319add014
dook0 <dook0@hotmail.com>
2013-07-17 11:11:47 +0200

    -a

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function(){
 	}
 
 	// The URL of your web server (the port is set in app.js)
-	var url = 'http://ec2-54-229-1-195.eu-west-1.compute.amazonaws.com:8333';
+	var url = 'http://localhost:8333';
 
 	var doc = $(document),
 		win = $(window),
