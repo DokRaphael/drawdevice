69ad6c7fd72ff0def1706074b6f3e80b6e9f28ac
dook0 <dook0@hotmail.com>
2013-07-29 15:17:28 +0200

    orientationhandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ $(function()
         
 	// The URL of your web server (the port is set in app.js)
 	
-	var url = 'http://ec2-54-229-102-239.eu-west-1.compute.amazonaws.com/';
+	var url = 'http://ec2-54-229-102-239.eu-west-1.compute.amazonaws.com/draw';
 	//var url = 'http://127.0.0.1/';
 	var doc = $(document),
 		win = $(window),
