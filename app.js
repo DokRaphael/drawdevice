a9bab1ef4437535a73add6c8aa13a4b6c7730be2
dook0 <dook0@hotmail.com>
2013-07-29 15:22:29 +0200

    pathdrawHandler

--- app.js
+++ app.js
@@ -99,9 +99,13 @@ app.get('/'+myData[newserver], function(req, res) {
 	res.sendfile(__dirname + '/assets/index.html' );    
 });*/
 
-app.get('/draw', function (req, res)
+app.get('/', function (req, res)
 {
 	res.sendfile(__dirname + '/draw/assets/index.html' );    
+});
+app.get('/draw', function (req, res)
+{
+	res.sendfile(__dirname + '/assets/index.html' );    
 });
 
 app.use(express.static(path.join(__dirname, 'assets')));
